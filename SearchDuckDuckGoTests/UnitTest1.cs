8a8d8f3953e49b6cefb9008a723414cf3ad35651
Юрій Сторож <storozh.yura@gmail.com>
2022-01-14 16:10:45 +0200

    Checked the additional display of the result

--- SearchDuckDuckGoTests/UnitTest1.cs
+++ SearchDuckDuckGoTests/UnitTest1.cs
@@ -22,7 +22,6 @@ namespace SearchDuckDuckGoTests
         private readonly By _searchMoreButton = By.XPath("//a[@class='result--more__btn btn btn--full']");
         private readonly By _search = By.XPath(" //a[@data-testid='result-title-a']");
        
-
         [SetUp]
         public void Setup()
         {
@@ -34,8 +33,7 @@ namespace SearchDuckDuckGoTests
         [Test]
         public void Test1()
         {
-
-           
+ 
             var searchInput = driver.FindElement(_searchInputButton);
             Thread.Sleep(500);
 
@@ -44,25 +42,37 @@ namespace SearchDuckDuckGoTests
             var search = driver.FindElement(_searchButton);
             Thread.Sleep(500);
             search.Click();
-            var collectionLinks = driver.FindElements(_search) //Получение коллекции
-           .Where(webElement => (webElement.Displayed == true)); //Фильтрация
+            var collectionLinks = driver.FindElements(_search) //Получення колекції
+           .Where(webElement => (webElement.Displayed == true)); //Фільтрація
             int i = 0;
             foreach (var link in collectionLinks)
             {
                 i++;
 
             }
-            Console.WriteLine($"{i}");
+            Console.WriteLine($"The display shows {i} search results");
             var searchMore = driver.FindElement(_searchMoreButton);
             Thread.Sleep(500);
             searchMore.Click();
             Thread.Sleep(500);
+            collectionLinks = driver.FindElements(_search) //Получення колекції
+           .Where(webElement => (webElement.Displayed == true)); //Фільтрація
+            int x = 0;
+            foreach (var link in collectionLinks)
+            {
+                x++;
 
-            
+            }
+            Console.WriteLine($"The display shows {x} search results after click 'More Resulst' button");
+            if(x>i)
+            {
+                Console.WriteLine("Additional test results are displayed");
+            }
+            else
+            {
+                Console.WriteLine("Additional test results are not displayed");
+            }
         }
-
-     
-
         [TearDown]
         public void TearDown()
         {
